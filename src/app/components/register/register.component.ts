6635b8c945abded177a7297eee902aa3a5b9227a
Marija123 <marijadjurkovic503@gmail.com>
2019-08-15 17:34:26 +0200

    initial commit client angular

---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { TokenPayload, AuthenticationService } from 'src/app/services/authentication/authentication.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  credentials: TokenPayload = {
+    email: '',
+    name: '',
+    password: '',
+    surname: '',
+    address: '',
+    birthday: new Date(),
+    image: '',
+    activated: '',
+    role: ''
+  };
+  constructor(private auth: AuthenticationService, private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  register() {
+    this.auth.register(this.credentials).subscribe(() => {
+      this.router.navigateByUrl('/profile');
+    }, (err) => {
+      console.error(err);
+    });
+  }
+
+}
